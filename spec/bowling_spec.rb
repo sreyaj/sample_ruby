ee4eab3dc1260b81d7e43362387a5a234d451391
buildsample <buildsampletest@gmail.com>
2014-11-17 13:19:13 +0530

    Update bowling_spec.rb

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -12,13 +12,13 @@ describe Bowling, "#category" do
    it "returns the correct category" do
   bowling = Bowling.new
   20.times { bowling.hit(0) }
-  bowling.category.should eql "category"
+  bowling.category.should eql "category1"
    end
 end
 describe Bowling, "#title" do
    it "returns the correct title" do
   bowling = Bowling.new
   20.times { bowling.hit(0) }
-  bowling.title.should eql "title"
+  bowling.title.should eql "title1"
    end
 end
